b526ea2713c551dfa9bd11faafba38ee41885a2c
Nikhil Rane <nikhilraorane95@gmail.com>
2023-07-02 13:27:18 +0530

    day 4 completed

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +16,13 @@ class MyApp extends StatelessWidget {
       // home: const Homepage(),
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(brightness: Brightness.dark),
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
       routes: {
         "/": (context) => const LoginPage(),
         "/login": (context) => const LoginPage()
-      },
+      },  
     );
   }
 }
